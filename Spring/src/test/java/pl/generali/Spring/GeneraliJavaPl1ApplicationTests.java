c5a2e1b67fb96c5dbd7d337b2e6da2111a758806
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-09-07 13:44:51 +0200

    Init spring project

---
+++ Spring/src/test/java/pl/generali/Spring/GeneraliJavaPl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.generali.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneraliJavaPl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
